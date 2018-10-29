0d0facea933a8dc89f20b5aacd2c0ee662c28a10
untungtanujaya <utanujaya@gmail.com>
2018-10-29 12:43:49 +0700

    init ack

---
+++ Ack.cpp
@@ -0,0 +1,31 @@
+/* Sliding Window Acknowledgement
+13516120 Nuha Adinata
+13516135 Untung Tanujaya
+13516141 Ilham Wahabi
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+class Ack {
+private:
+    char ack;
+    int nextSeqNum;
+    char checksum;
+public:
+    Ack(char ack, int nextSeqNum, char checksum) {
+        this->ack = ack;
+        this->nextSeqNum = nextSeqNum;
+        this->checksum = checksum;
+    }
+
+    ~Ack() = default;
+
+    // operations
+
+    // bool isValid () {
+    //
+    // }    
+};
