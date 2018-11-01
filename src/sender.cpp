d80b572b87d2f9fccbebf912b0b58ffd7a1cf6ee
IlhamWahabiGX <wahabiputra@gmail.com>
2018-11-01 12:53:50 +0700

    Update project structure

--- src/sender.cpp
+++ src/sender.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <stdlib.h>
+#include <thread>
+#include <mutex>
+#include <stdio.h>
 #include <string>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <stdexcept>
 #include <unistd.h>
+
 #include "Packet.cpp"
 #include "Ack.cpp"
+
 using namespace std;
 
 void die(string msg) {
